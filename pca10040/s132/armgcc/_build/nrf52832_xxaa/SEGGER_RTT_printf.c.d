e50e311b3eb5c082b4841f9b8a5f08638959e4ba
allfake <allfake24@gmail.com>
2019-03-13 17:54:32 +0700

    first commit

---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa/SEGGER_RTT_printf.c.d
@@ -0,0 +1,170 @@
+_build/nrf52832_xxaa/SEGGER_RTT_printf.c.o: \
+ ../../../../../../external/segger_rtt/SEGGER_RTT_printf.c \
+ ../../../../../../external/segger_rtt/SEGGER_RTT.h \
+ ../../../../../../external/segger_rtt/SEGGER_RTT_Conf.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_util_platform.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_nvic.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdio.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/newlib.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/config.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/cdefs.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stddef.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdarg.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/reent.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_types.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/_types.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/lock.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/types.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/types.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/stdio.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../config/sdk_config.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdlib.h \
+ /Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/stdlib.h
+
+../../../../../../external/segger_rtt/SEGGER_RTT.h:
+
+../../../../../../external/segger_rtt/SEGGER_RTT_Conf.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_util_platform.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdint.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/features.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_newlib_version.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error_soc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_nvic.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdio.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_ansi.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/newlib.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/config.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/cdefs.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stddef.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdarg.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/reent.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/_ansi.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_types.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/_types.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/lock.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/types.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/types.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/sys/stdio.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
+
+../config/sdk_config.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/stdlib.h:
+
+/Users/admin/CMMC/gcc-arm-none-eabi/arm-none-eabi/include/machine/stdlib.h:
